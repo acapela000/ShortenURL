a4d5b97d486a2c8827bbaa212e32da9b60fb2bcd
thao <mrtrados@gmail.com>
2023-06-06 22:31:46 +0900

    ADD annotation

--- src/main/java/today/whatisthaodoing/ShortenURL/model/ShortenURL.java
+++ src/main/java/today/whatisthaodoing/ShortenURL/model/ShortenURL.java
@@ -3,11 +3,19 @@ package today.whatisthaodoing.ShortenURL.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @JsonSerialize
 @JsonDeserialize
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Document(collation = "shortURL")
 public class ShortenURL {
 
@@ -18,27 +26,7 @@ public class ShortenURL {
     @JsonProperty
     private String shortedURL;
 
-    public ShortenURL() {
-    }
 
-    public ShortenURL(String originalURL, String shortedURL) {
-        this.originalURL = originalURL;
-        this.shortedURL = shortedURL;
-    }
-
-    public String getOriginalURL() {
-        return originalURL;
-    }
-
-    public void setOriginalURL(String originalURL) {
-        this.originalURL = originalURL;
-    }
-
-    public String getShortedURL() {
-        return shortedURL;
-    }
-
-    public void setShortedURL(String shortedURL) {
-        this.shortedURL = shortedURL;
-    }
 }
+
+
